5d44e032b1ecaab6eb28d100c575f4ae305c021b
yogendra-rathore <140323526+yogendra-rathore@users.noreply.github.com>
2024-02-10 17:04:57 +0530

    Update sendMail.js

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 
-const sendMail=async (options)=>{
+const sendMailWithoutFile=async (options)=>{
     const transporter=nodemailer.createTransport({
         host: process.env.SMPT_HOST,
         port:process.env.SMPT_PORT,
@@ -21,4 +21,35 @@ const sendMail=async (options)=>{
     await transporter.sendMail(mailOptions);
 }
 
-module.exports=sendMail;
+const sendMailWithFiles=async (options)=>{
+    const transporter=nodemailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port:process.env.SMPT_PORT,
+        service: process.env.SMPT_SERVICE,
+        auth:{
+            user: process.env.SMPT_MAIL,
+            pass: process.env.SMPT_PASSWORD
+        }
+
+    });
+    const mailOptions={
+        from: process.env.SMPT_MAIL,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        attachments: [
+            {
+              filename: 'receipt.pdf',
+              path: options.filePath,
+              encoding: 'base64',
+            },
+          ]
+    }
+
+    await transporter.sendMail(mailOptions);
+}
+
+module.exports={
+    sendMailWithoutFile,
+    sendMailWithFiles,
+  };
