b4f099c1253c2b1df90ff5055af2d453c3c3ab38
Anuj Pratap Singh <anujjadon98@gmail.com>
2024-11-06 20:41:48 +0530

    remove cors

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Socket.io setup with CORS
-const io = socketIo(server, {
-    cors: {
-        origin: ['http://localhost:3000', 'https://peppy-praline-354922.netlify.app/', 'http://localhost:3001'],
-        credentials: true
-    }
-});
+const io = socketIo(server);
 
 // Initialize socket connections
 io.on('connection', (socket) => {
@@ -47,11 +42,11 @@ io.on('connection', (socket) => {
 initializeOrderSocket(io);
 initializeNotificationSocket(io)
 
-// Setting up middlewares
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://peppy-praline-354922.netlify.app/', 'http://localhost:3001'],
-    credentials: true
-}));
+// // Setting up middlewares
+// app.use(cors({
+//     origin: ['http://localhost:3000', 'https://peppy-praline-354922.netlify.app', 'http://localhost:3001'],
+//     credentials: true
+// }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
